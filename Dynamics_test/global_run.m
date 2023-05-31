bcaadff2fcd9974790289bb9b8c9bd690f5439df
Kaymie Shiozawa <kaymie0120@gmail.com>
2023-05-31 13:26:02 -0400

    duarte young fit

--- Dynamics_test/global_run.m
+++ Dynamics_test/global_run.m
@@ -3,7 +3,7 @@ clear all;
 clc;
 setpath
 %% Load Human Data
-subject_type = 'duarte_old';
+subject_type = 'duarte_young';
 human_struct = load(sprintf('%s.mat',subject_type));
 input.Frequency = human_struct.Frequency;
 input.FrequencyWindow = 0.2;
@@ -46,7 +46,7 @@ params.sigma_r = [0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.
 %     length(gamma),length(kappa),length(eta));\
 % load('C:\Users\kaymi\Dropbox (MIT)\Balance Project\Intersection Point Analysis\ip_analysis\Dynamics_test\Data\Error\rmse20230223_3.mat')
 % rmse_nonpar7sub_lqr_6params = Error;
-rmse_old_lqr = zeros(length(params.beta),length(params.sigma_r));
+rmse_young_lqr = zeros(length(params.beta),length(params.sigma_r));
 %% Run Simulation for n trials across different parameter values
 num_trial = 40;
 count = 0;
@@ -63,7 +63,7 @@ for b = 1:length(params.beta)
         [ip,~] = compute_ip(input,num_trial);
         ip_filtered = neglect_outliers(ip);
         meansim = mean(ip_filtered,'omitnan');
-        rmse_old_lqr(b,s) = sqrt(mean((meansim-human_struct.IPDataAverage).^2));
+        rmse_young_lqr(b,s) = sqrt(mean((meansim-human_struct.IPDataAverage).^2));
         %             figure(1);
         %             plot(f,human)
         %             hold on
@@ -75,4 +75,4 @@ for b = 1:length(params.beta)
     end
 end
 toc
-save_error_file(input,subject_type,num_trial,params,'rmse_old_20230526',rmse_old_lqr)
+save_error_file(input,subject_type,num_trial,params,'rmse_young_20230530',rmse_young_lqr)
